afa542c0b1e72bc132f68f554ddfb35d421487f0
yang <joey674901205@gmail.com>
2022-07-27 14:47:26 -0400

    follow guide

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
+const sequelize = require('../../config/connection');
+const sequilize = require('../../config/connection');
 
 router.get('/', (req, res) => {
   console.log('=======================');
@@ -11,7 +13,15 @@ router.get('/', (req, res) => {
         model: User,
         attributes: ['username']
       }
-    ]
+    ],
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+
   })
     .then(dbPostData => res.json(dbPostData))
     .catch(err => {
@@ -25,7 +35,13 @@ router.get('/:id', (req, res) => {
     where: {
       id: req.params.id
     },
-    attributes: ['id', 'post_url', 'title', 'created_at'],
+    attributes: [
+      'id', 
+      'post_url', 
+      'title', 
+      'created_at', 
+      [sequilize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']  
+    ],
     include: [
       {
         model: User,
@@ -57,6 +73,16 @@ router.post('/', (req, res) => {
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
+    });
+});
+
+router.put('/upvote', (req, res) => {
+  // custom static method created in models/Post.js
+  Post.upvote(req.body, { Vote })
+    .then(updatedPostData => res.json(updatedPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
     });
 });
 
